8c489fd1f88c2ba942aa6c9e3a08e763e86fd947
Ruslan <noname0263@gmail.com>
2024-02-18 03:12:48 +0700

    init App

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -1,14 +1,38 @@
 package edu.java.bot;
 
-import edu.java.bot.configuration.ApplicationConfig;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import edu.java.bot.controllers.BotController;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import static edu.java.bot.utilities.Token.getTelegramToken;
 
+@Log4j2
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationConfig.class)
+//@EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(BotApplication.class, args);
+
+        String BOT_TOKEN = getTelegramToken();
+        log.debug(BOT_TOKEN);
+        var bot = new TelegramBot(BOT_TOKEN);
+
+        bot.setUpdatesListener(updates -> {
+            log.info(updates);
+            BotController.process(updates, bot);
+
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        }, e -> {
+            if (e.response() != null) {
+                // got bad response from telegram
+                e.response().errorCode();
+                e.response().description();
+            } else {
+                // probably network error
+                log.info(e.getStackTrace());
+            }
+        });
     }
 }
