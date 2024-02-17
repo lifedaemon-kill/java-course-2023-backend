cadba9b3ecfbcf103ea2fae6bfb97c3b4542f6dd
Ruslan <noname0263@gmail.com>
2024-02-18 03:26:25 +0700

    add tests

--- bot/src/test/java/edu/LinkParse/IsSupportsTest.java
+++ bot/src/test/java/edu/LinkParse/IsSupportsTest.java
@@ -1,29 +1,36 @@
 package edu.LinkParse;
 
 import edu.java.bot.utilities.LinkParse;
+import java.net.URISyntaxException;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Log4j2
 public class IsSupportsTest {
     @Test
-    public void test1() throws URISyntaxException {
-        URI url = new URI("https://stackoverflow.com/questions/123456");
-
+    public void test1() {
+        String url = "https://stackoverflow.com/questions/123456";
         boolean response = LinkParse.isURLSupports(url);
+        assertThat(response).isTrue();
+    }
 
+    @Test
+    public void test2() {
+        String url = "https://meta.stackoverflow.com/questions/123456";
+        boolean response = LinkParse.isURLSupports(url);
+        assertThat(response).isFalse();
+    }
+    @Test
+    public void test3() {
+        String url = "https://github.com/questions/1256";
+        boolean response = LinkParse.isURLSupports(url);
         assertThat(response).isTrue();
     }
     @Test
-    public void test2() throws URISyntaxException {
-        URI url = new URI("https://meta.stackoverflow.com/questions/123456");
-
+    public void test4() {
+        String url = "1256";
         boolean response = LinkParse.isURLSupports(url);
-
         assertThat(response).isFalse();
     }
 }
--- bot/src/test/java/edu/LinkParse/IsURLTest.java
+++ bot/src/test/java/edu/LinkParse/IsURLTest.java
@@ -1,4 +1,21 @@
 package edu.LinkParse;
 
+import edu.java.bot.utilities.LinkParse;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class IsURLTest {
+    @Test
+    public void test1() {
+        String url = "https://stackoverflow.com/questions/123456";
+        boolean response = LinkParse.isURL(url);
+        assertThat(response).isTrue();
+    }
+
+    @Test
+    public void test2() {
+        String url = "questions/123456";
+        boolean response = LinkParse.isURL(url);
+        assertThat(response).isFalse();
+    }
 }
