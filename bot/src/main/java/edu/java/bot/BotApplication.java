80ee855f192a909b4ecb4d8742118596466ab20e
Ruslan <noname0263@gmail.com>
2024-02-26 00:45:26 +0700

    make more simple code

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -2,27 +2,37 @@ package edu.java.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import edu.java.bot.configurations.ApplicationConfig;
-import edu.java.bot.controllers.Controller;
-import edu.java.bot.models.DataBase;
+import edu.java.bot.configuration.ApplicationConfig;
+import edu.java.bot.controller.Controller;
+import edu.java.bot.model.DataBase;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static edu.java.bot.utilities.Token.getTelegramToken;
+import org.springframework.stereotype.Component;
+import static edu.java.bot.utility.Token.getFromFileTelegramToken;
 
 @Log4j2
+@Component
 @SpringBootApplication
 //@EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
-    public static void main(String[] args) throws Exception {
+    @Autowired
+    private static ApplicationConfig appConfig = new ApplicationConfig();
+
+    static {
+        appConfig.setTelegramToken(getFromFileTelegramToken());
+    }
+
+    @Autowired
+    private static TelegramBot bot = new TelegramBot(appConfig.getTelegramToken());
+    @Autowired
+    private static DataBase dataBase = new DataBase();
+
+    public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
 
-        ApplicationConfig config = new ApplicationConfig(getTelegramToken());
-
-        var bot = new TelegramBot(config.telegramToken());
         log.debug(bot.getToken());
-        DataBase dataBase = new DataBase();
-
         bot.setUpdatesListener(updates -> {
             //log.info(updates);
             Controller.process(updates, bot, dataBase);
@@ -35,7 +45,7 @@ public class BotApplication {
                 e.response().description();
             } else {
                 // probably network error
-                log.info(e.getStackTrace());
+                log.error(e.getStackTrace());
             }
         });
     }
