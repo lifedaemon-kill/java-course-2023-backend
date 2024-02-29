3299fcd65a9f8dd6e6d81a14e46c5574822f4e3f
Ruslan <noname0263@gmail.com>
2024-02-29 18:44:53 +0700

    add Service class and optimise structure

--- scrapper/src/main/java/edu/java/service/GitHubService.java
+++ scrapper/src/main/java/edu/java/service/GitHubService.java
@@ -1,20 +1,20 @@
 package edu.java.service;
 
+import edu.java.client.GitHubClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
-public class GitHubService {
-    private final WebClient webClient;
-
-    public GitHubService(WebClient webClient) {
-        this.webClient = webClient;
+public class GitHubService extends edu.java.service.Service {
+    public GitHubService(GitHubClient client) {
+        super(client);
     }
 
-    public Mono<String> getRepository(String repo) {
-        return webClient.get()
-            .uri("/repos/{repo}", repo)
+    @Override
+    public Mono<String> getRepository(String repository) {
+        return client
+            .webClient.get()
+            .uri("/repos/{repository}", repository)
             .retrieve()
             .bodyToMono(String.class);
     }
