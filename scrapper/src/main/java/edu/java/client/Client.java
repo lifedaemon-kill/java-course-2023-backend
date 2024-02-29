ef4a50188d4e90644df098a2b229e3c674637fbe
Ruslan <noname0263@gmail.com>
2024-02-29 18:45:10 +0700

    updated modification

--- scrapper/src/main/java/edu/java/client/Client.java
+++ scrapper/src/main/java/edu/java/client/Client.java
@@ -3,7 +3,7 @@ package edu.java.client;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public abstract class Client {
-    protected final WebClient webClient;
+    public final WebClient webClient;
 
     protected Client(String baseURL) {
         this.webClient = WebClient.builder().baseUrl(baseURL).build();
