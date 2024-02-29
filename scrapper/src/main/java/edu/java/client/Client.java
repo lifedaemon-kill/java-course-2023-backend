5724010c836422baecf8b23e3411ccbc2b554a8b
Ruslan <noname0263@gmail.com>
2024-03-01 00:01:59 +0700

    add default value

--- scrapper/src/main/java/edu/java/client/Client.java
+++ scrapper/src/main/java/edu/java/client/Client.java
@@ -5,12 +5,12 @@ import reactor.core.publisher.Mono;
 
 public abstract class Client {
     public final WebClient webClient;
-    protected Client() {
-        this.webClient = WebClient.builder().baseUrl("http://localhost:").build();
-    }
+
     protected Client(String baseURL) {
         this.webClient = WebClient.builder().baseUrl(baseURL).build();
     }
-
+    public Client(){
+        this("http://localhost:");
+    }
     public abstract Mono<String> getRepository(String repository);
 }
