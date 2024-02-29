0600f7ed690571317933ae2b536dc02531686d65
Ruslan <noname0263@gmail.com>
2024-02-29 19:08:59 +0700

    optimised structure

--- scrapper/src/main/java/edu/java/client/Client.java
+++ scrapper/src/main/java/edu/java/client/Client.java
@@ -1,6 +1,7 @@
 package edu.java.client;
 
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 public abstract class Client {
     public final WebClient webClient;
@@ -8,4 +9,6 @@ public abstract class Client {
     protected Client(String baseURL) {
         this.webClient = WebClient.builder().baseUrl(baseURL).build();
     }
+
+    public abstract Mono<String> getRepository(String repository);
 }
--- scrapper/src/main/java/edu/java/client/GitHubClient.java
+++ scrapper/src/main/java/edu/java/client/GitHubClient.java
@@ -1,10 +1,19 @@
 package edu.java.client;
 
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 @Component
 public class GitHubClient extends Client {
     public GitHubClient() {
         super("https://api.github.com");
+    }
+
+    @Override
+    public Mono<String> getRepository(String repository) {
+        return webClient.get()
+            .uri("/repos/{repository}", repository)
+            .retrieve()
+            .bodyToMono(String.class);
     }
 }
