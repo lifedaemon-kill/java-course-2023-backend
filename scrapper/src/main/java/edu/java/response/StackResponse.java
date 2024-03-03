17e54883267841d9f37526aa22510e1b23b03396
Ruslan <noname0263@gmail.com>
2024-03-03 23:55:23 +0700

    created data transport classes

--- scrapper/src/main/java/edu/java/response/StackResponse.java
+++ scrapper/src/main/java/edu/java/response/StackResponse.java
@@ -1,24 +1,26 @@
 package edu.java.response;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.java.model.TopicState;
 import java.time.OffsetDateTime;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class GitResponse extends Response {
+public class StackResponse extends Response {
     @Override
     public int getAnswersCount(String json) throws WrongThreadException {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readTree(json).size();
+            JsonNode jsonNode = objectMapper.readTree(json);
+            return jsonNode.get("answer_count").asInt();
         } catch (Exception e) {
             log.error(e);
             throw new WrongThreadException();
         }
     }
 
-    public GitResponse(String json) {
+    public StackResponse(String json) {
         this.data = new TopicState(
             OffsetDateTime.now(),
             this.getAnswersCount(json)
