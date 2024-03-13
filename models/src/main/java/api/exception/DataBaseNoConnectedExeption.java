075cfbc26f81828736370dccd57c57fa4fc627f2
Ruslan <noname0263@gmail.com>
2024-03-14 05:03:45 +0700

    add new Exceptions

---
+++ models/src/main/java/api/exception/DataBaseNoConnectedExeption.java
@@ -0,0 +1,4 @@
+package api.exception;
+
+public class DataBaseNoConnectedExeption extends RuntimeException{
+}
---
+++ models/src/main/java/api/exception/NotRegisteredException.java
@@ -0,0 +1,4 @@
+package api.exception;
+
+public class NotRegisteredException extends RuntimeException{
+}
