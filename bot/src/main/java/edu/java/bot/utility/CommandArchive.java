54b335a36b6dac0927db67a111796dc8e9826c6d
Ruslan <noname0263@gmail.com>
2024-03-16 20:09:51 +0700

    rename commands string values

--- bot/src/main/java/edu/java/bot/utility/CommandArchive.java
+++ bot/src/main/java/edu/java/bot/utility/CommandArchive.java
@@ -3,13 +3,13 @@ package edu.java.bot.utility;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StringCommand {
+public class CommandArchive {
     public static final String START = "/start";
     public static final String TRACK = "/track";
     public static final String UNTRACK = "/untrack";
     public static final String HELP = "/help";
     public static final String LIST = "/list";
 
-    private StringCommand() {
+    private CommandArchive() {
     }
 }
