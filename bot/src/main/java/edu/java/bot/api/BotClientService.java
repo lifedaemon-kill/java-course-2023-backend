620c658c4f33da245fe1246b356fd48672f13d4e
Ruslan <noname0263@gmail.com>
2024-03-16 20:06:03 +0700

    add bot http service

---
+++ bot/src/main/java/edu/java/bot/api/BotClientService.java
@@ -0,0 +1,37 @@
+package edu.java.bot.api;
+
+import api.errorResponse.ApiErrorResponse;
+import database.DialogState;
+import dto.response.DialogStateResponse;
+import edu.java.bot.api.httpclient.BotHttpClient;
+import jakarta.validation.constraints.Null;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+import java.util.Objects;
+
+@Log4j2
+@Service
+public class BotClientService {
+    private final BotHttpClient client;
+
+    public BotClientService(BotHttpClient client) {
+        this.client = client;
+    }
+
+    public Mono<DialogStateResponse> getDialogState(Long id) {
+        return client.webClient
+            .get()
+            .uri("/state")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .header("Tg-Chat-Id", id.toString())
+            .retrieve()
+            .bodyToMono(DialogStateResponse.class);
+    }
+}
---
+++ bot/src/main/java/edu/java/bot/api/httpclient/BotHttpClient.java
@@ -0,0 +1,12 @@
+package edu.java.bot.api.httpclient;
+
+import edu.java.bot.configuration.BotHttpClientConfig;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class BotHttpClient {
+    public WebClient webClient;
+
+    public BotHttpClient(BotHttpClientConfig config){
+        this.webClient = WebClient.create(config.getBaseURL());
+    }
+}
