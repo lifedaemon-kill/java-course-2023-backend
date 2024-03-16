5de341fd0aea00a3f21ff31c922a01a94ffd21fc
Ruslan <noname0263@gmail.com>
2024-03-16 20:04:33 +0700

    updated bot's beans

--- bot/src/main/java/edu/java/bot/configuration/BotConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/BotConfig.java
@@ -1,8 +1,10 @@
 package edu.java.bot.configuration;
 
+import com.pengrad.telegrambot.ExceptionHandler;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import edu.java.bot.controller.Controller;
+import edu.java.bot.api.httpclient.BotHttpClient;
+import edu.java.bot.controller.UpdatesController;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,22 +13,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BotConfig {
     @Bean
-    public TelegramBot telegramBot(ApplicationConfig appConfig) {
-        TelegramBot bot = new TelegramBot(appConfig.getTelegramToken());
-
-        bot.setUpdatesListener(updates -> {
-            Controller.process(updates, bot, appConfig.getDataBase());
-
+    public UpdatesListener updatesListener() {
+        return updates -> {
+            UpdatesController.process(updates);
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
-        }, e -> {
+        };
+    }
+
+    @Bean
+    public ExceptionHandler exceptionHandler() {
+        return e -> {
             if (e.response() != null) {
                 e.response().errorCode();
                 e.response().description();
             } else {
                 log.error(e.getMessage(), e);
             }
-        });
+        };
+    }
 
+    @Bean
+    public TelegramBot telegramBot(
+        ApplicationConfig appConfig,
+        BotHttpClient httpClient,
+        UpdatesListener updatesListener,
+        ExceptionHandler exceptionHandler
+    ) {
+        TelegramBot bot = new TelegramBot(appConfig.getTelegramToken());
+        bot.setUpdatesListener(updatesListener, exceptionHandler);
         return bot;
     }
 }
