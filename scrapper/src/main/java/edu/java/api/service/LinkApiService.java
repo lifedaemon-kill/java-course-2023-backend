e68fcc2218da070b60b08b3abc5ccdd489652076
Ruslan <noname0263@gmail.com>
2024-04-08 15:46:13 +0700

    updated "add" and "remove" methods

--- scrapper/src/main/java/edu/java/api/service/LinkApiService.java
+++ scrapper/src/main/java/edu/java/api/service/LinkApiService.java
@@ -1,5 +1,6 @@
 package edu.java.api.service;
 
+import api.exception.DataBaseNoConnectedException;
 import api.exception.LinkAlreadyAddedException;
 import api.exception.NotFoundException;
 import dto.request.AddLinkRequest;
@@ -10,13 +11,16 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Log4j2
 @Service
 public class LinkApiService extends AbstractApiService {
     //Link
+
     public LinkResponse addLinkTracking(Long id, AddLinkRequest request) {
         URI link = request.link();
         if (!isChatExist(id)) {
@@ -25,30 +29,57 @@ public class LinkApiService extends AbstractApiService {
 
         try {
             linkRepository.add(link);
-        } catch (DuplicateKeyException ignored) {
+        } catch (DataAccessException ignored) {
+            log.info("duplicated url");
         }
-        Link linkEntity = linkRepository.findByUrl(link);
+
+        Link linkEntity;
         try {
-            relationRepository.add(linkEntity.getId(), id);
-        } catch (DuplicateKeyException e) {
-            throw new LinkAlreadyAddedException();
+            linkEntity = linkRepository.findByUrl(link);
+        } catch (DataAccessException e) {
+            log.error("Не нашлось по url");
+            throw new DataBaseNoConnectedException();
+        }
+
+        try {
+            log.info(linkEntity.getId() + " " + linkEntity.getUrl() + "   " + id);
+            relationRepository.findOne(linkEntity.getId(), id);
+        } catch (EmptyResultDataAccessException e) {
+            log.error("Не нашел в relation findone");
+            try {
+                relationRepository.add(linkEntity.getId(), id);
+            } catch (DataAccessException ignored) {
+                log.error("duplicated relation");
+                throw new LinkAlreadyAddedException();
+            }
         }
 
         return new LinkResponse(id, request.link());
     }
 
+
     public LinkResponse deleteLinkTracking(Long id, URI url) {
         if (!isChatExist(id)) {
             throw new NotFoundException();
         }
+
         Link linkEntity = linkRepository.findByUrl(url);
         if (linkEntity == null) {
             throw new NotFoundException();
         }
 
-        if (relationRepository.remove(linkEntity.getId(), id) == 0) {
+        if (relationRepository.findOne(linkEntity.getId(), id) == null) {
             throw new NotFoundException();
         }
+
+        try {
+            relationRepository.remove(linkEntity.getId(), id);
+
+        } catch (Exception e) {
+
+            throw new NotFoundException();
+        }
+
         return new LinkResponse(id, url);
     }
 
