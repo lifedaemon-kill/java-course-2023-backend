cd3a75a5f2a3d2d97bf218027d1f7f6725f520ca
Ruslan <noname0263@gmail.com>
2024-04-20 10:20:43 +0700

    add Jpa repositories

---
+++ scrapper/src/main/java/edu/java/domain/jpa/JpaChatRepository.java
@@ -0,0 +1,59 @@
+package edu.java.domain.jpa;
+
+import edu.java.domain.interfaces.ChatRepository;
+import edu.java.entity.Chat;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.time.OffsetDateTime;
+import java.util.Collection;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
+
+@Log4j2
+@Repository
+public class JpaChatRepository implements ChatRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void add(Long id) {
+        Chat chat = new Chat();
+        chat.setId(id);
+        chat.setState(0);
+        chat.setLastUpdateAt(OffsetDateTime.now());
+        em.persist(chat);
+    }
+
+    @Override
+    public void update(Long id, int state) {
+        Chat chat = em.find(Chat.class, id);
+        if (chat != null) {
+            chat.setState(state);
+            chat.setLastUpdateAt(OffsetDateTime.now());
+            em.merge(chat);
+        } else {
+            log.error("Jpa chat update did not find chat with id {}", id);
+        }
+    }
+
+    @Override
+    public void remove(Long id) {
+        Chat chat = em.find(Chat.class, id);
+        if (chat != null) {
+            em.remove(chat);
+        } else {
+            log.error("Jpa chat remove did not find chat with id {}", id);
+        }
+    }
+
+    @Override
+    public Collection<Chat> findAll() {
+        String query = "SELECT c FROM Chat c";
+        return em.createQuery(query, Chat.class).getResultList();
+    }
+
+    @Override
+    public Chat findById(Long id) {
+        return em.find(Chat.class, id);
+    }
+}
